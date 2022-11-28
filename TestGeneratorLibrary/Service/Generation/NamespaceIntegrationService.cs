48da38f6d80883bfb62a3365afd382586a74f572
Anton <antondanik32@mail.ru>
2022-11-29 00:31:30 +0300

    Create generation services.

---
+++ TestGeneratorLibrary/Service/Generation/NamespaceIntegrationService.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TestGeneratorLibrary.Model;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace TestGeneratorLibrary.Service.Generation
+{
+    public class NamespaceIntegrationService
+    {
+
+        private readonly ClassGenerationService _classGenerationService;
+
+        public NamespaceIntegrationService(ClassGenerationService classGenerationService)
+        {
+            _classGenerationService = classGenerationService;
+        }
+        
+        public ClassInformation GenerateTestClassWithNamespaces(ClassDeclarationSyntax classDeclaration)
+        {
+            SyntaxNode currentTreeNode = classDeclaration;
+            ClassDeclarationSyntax testClassDeclaration = _classGenerationService.GenerateTestsClass(classDeclaration);
+            SyntaxNode currentNamespace = currentTreeNode;
+            StringBuilder testClassDeclarationBuilder = new StringBuilder(testClassDeclaration.Identifier.Text);
+            
+            
+            NamespaceDeclarationSyntax parent = (NamespaceDeclarationSyntax)currentTreeNode.Parent;
+            NamespaceDeclarationSyntax ns = NamespaceDeclaration(IdentifierName(parent.Name + ".Tests"))
+                .WithMembers(new SyntaxList<MemberDeclarationSyntax>(testClassDeclaration));;
+            
+            currentNamespace = ns;
+            testClassDeclarationBuilder.Insert(0, $"{ns.Name}.");
+            currentTreeNode = currentTreeNode.Parent;
+            
+            while (currentTreeNode.Parent is NamespaceDeclarationSyntax)
+            {
+                parent = (NamespaceDeclarationSyntax)currentTreeNode.Parent;
+
+                ns = NamespaceDeclaration(parent.Name)
+                    .WithMembers(new SyntaxList<MemberDeclarationSyntax>((NamespaceDeclarationSyntax)currentNamespace));
+
+                currentNamespace = ns;
+                testClassDeclarationBuilder.Insert(0, $"{ns.Name}.");
+                currentTreeNode = currentTreeNode.Parent;
+            }
+
+            return new ClassInformation(testClassDeclarationBuilder.ToString(), (MemberDeclarationSyntax)currentNamespace);
+        }
+    }
+}
---
+++ TestGeneratorLibrary/Service/Generation/TestGeneratorService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TestGeneratorLibrary.Model;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace TestGeneratorLibrary.Service.Generation
+{
+    public class TestGeneratorService
+    {
+
+        private readonly NamespaceIntegrationService _namespaceIntegrationService;
+
+        public TestGeneratorService(NamespaceIntegrationService namespaceIntegrationService)
+        {
+            _namespaceIntegrationService = namespaceIntegrationService;
+        }
+
+        public List<FileInformation> Generate(string source)
+        {
+            CompilationUnitSyntax root = CSharpSyntaxTree.ParseText(source).GetCompilationUnitRoot();
+            List<ClassDeclarationSyntax> classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>()
+                .Where(node => node.Modifiers.Any(n => n.Kind() == SyntaxKind.PublicKeyword)).ToList();
+            
+            List<ClassInformation> classesDeclarations = classes.Select(_namespaceIntegrationService.GenerateTestClassWithNamespaces).ToList();
+            return GenerateFileInformation(classesDeclarations);
+        }
+
+        private List<FileInformation> GenerateFileInformation(List<ClassInformation> classesDeclarations)
+        {
+            return classesDeclarations.Select(classData => new FileInformation(classData.ClassName,
+                CompilationUnit()
+                    .WithUsings(new SyntaxList<UsingDirectiveSyntax>()
+                        .Add(UsingDirective(QualifiedName(IdentifierName("NUnit"), IdentifierName("Framework"))))
+                    )
+                    .AddMembers(classData.TestClassDeclarationSyntax)
+                    .NormalizeWhitespace().ToFullString())).ToList();
+        }
+    }
+}
